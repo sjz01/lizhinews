99cbb1b1cfcc4852ce4db22bea86e617cb27aa94
AIfriend <13838200094@163.com>
2019-07-26 14:19:55 +0800

    (新闻类型导航页)未完成

--- src/components/Newlines.vue
+++ src/components/Newlines.vue
@@ -1,21 +1,52 @@
 <template>
     <div id="newlines">
-        首页
+        <!-- 顶部样式 没有加滑动效果 -->
+
+        <div id="Topbar" >
+        <router-link  tag="span"  to="">国内</router-link>
+        <router-link  tag="span"  to="">国内</router-link> 
+        <router-link  tag="span"  to="">国际</router-link>  
+        <router-link  tag="span"  to="">互联网</router-link>   
+        <router-link  tag="span"  to="">体育</router-link>   
+        <router-link  tag="span"  to="">游戏</router-link>  
+        <router-link  tag="span"  to="">金融</router-link>   
+        <router-link  tag="span"  to="">娱乐</router-link>  
+        <router-link  tag="span"  to=""> 房地产</router-link>  
+       
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Newlines"
-    }
+
+
+
+
+
+export default {
+  name: "Newlines",
+ 
+};
 </script>
 
 <style lang="less" scoped>
-    #newlines{
-        margin-top: 80px;
-        background-color: red;
-        width: 100%;
-        height: 50px;
+#newlines {
+  margin-top: 80px;
+  background-color: #cccccc;
+  overflow: hidden;
 
+  #Topbar {
+    width: 450px;
+
+    background-color: white;
+
+    span{
+      display: inline-block;
+      height: 50px;
+      width: 50px;
+      line-height: 50px;
+      font-size: 16px;
     }
+  }
+}
 </style>
